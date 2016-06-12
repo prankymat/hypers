31454c6258282a4f87fab93ee6290bb9d7e4f821
yuuji <yuuji@yuujis-Mac-Pro.local>
2016-06-12 07:26:42 -0700

    light documentation

--- corehypers/transmit.swift
+++ corehypers/transmit.swift
@@ -15,7 +15,10 @@ let client = try! SXStreamClient(ip: "127.0.0.1", port: 5555, domain: .INET) { (
 
 client.start(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), initialPayload: nil)
 
+/* lie, no encryption yet */
 func encrypt(command: String) -> NSData {
+    
+    /* console */
     var count = command.characters.count + 1
     var argc = command.componentsSeparatedByString(" ").filter({!($0.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).characters.count == 0)}).count
     var payload = [UInt8](count: count + 2 * sizeof(Int) + 1, repeatedValue: 0)
@@ -24,6 +27,7 @@ func encrypt(command: String) -> NSData {
     payload[2 * sizeof(Int)] = 0x20;
     command.dataUsingEncoding(NSUTF8StringEncoding)!.getBytes(&payload[2 * sizeof(Int) + 1], length: sizeof(Int))
     print(payload.count)
+    
     return NSData(bytes: &payload, length: payload.count)
 }
 
