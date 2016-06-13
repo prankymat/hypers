a31ff882f89929e54e9f765b35f8f54eb7180a1c
yuuji <yuuji@yuujis-Mac-Pro.local>
2016-06-12 17:59:14 -0700

    url parsing

--- ios/hypers/NSURL+params.swift
+++ ios/hypers/NSURL+params.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+struct WebAPIAttribute {
+    var `protocol`: String // for example: http
+    var att: [String: String]
+}
 extension NSURL {
     var params: [String: String] {
         get {
@@ -32,5 +36,36 @@ extension String {
             }
             return items
         }
+    }
+    
+    
+    var urlAPIComponents: WebAPIAttribute? {
+        guard let pos = self.rangeOfString("://") else {return nil}
+        guard let s = self.rangeOfString("?") else {return nil}
+        
+        let `protocol` = self.substringWithRange(self.startIndex..<pos.startIndex)
+        
+        var ret = WebAPIAttribute(protocol: `protocol`, att: [:])
+        let str = self.substringWithRange(s.endIndex..<self.endIndex)
+        
+        for att in str.componentsSeparatedByString("&") {
+            let keyvalue = att.componentsSeparatedByString("=")
+            ret.att[keyvalue[0]] = keyvalue[1]
+        }
+        
+        return ret
+    }
+    
+    var urlAPIAttributes: [String: String]? {
+        guard let s = self.rangeOfString("?") else {return nil}
+        
+        var ret = [String: String]()
+        let str = self.substringWithRange(s.endIndex..<self.endIndex)
+        
+        for att in str.componentsSeparatedByString("&") {
+            let keyvalue = att.componentsSeparatedByString("=")
+            ret[keyvalue[0]] = keyvalue[1]
+        }
+        return ret
     }
 }
